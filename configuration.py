6892bf6c832dc000112132c8257ff2d555fe4a37
Yuliya Eryemina <rabota.office2016@yandex.ru>
2025-09-06 22:46:25 +0300

    изменение в задании 1,2. в файлах test_create_order_track.py, sender_stand_request.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = "https://07eb16da-3778-4d49-ac85-3b24e58a46fc.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://77c960a0-c9ef-4a19-940a-70e010959168.serverhub.praktikum-services.ru"
 # Создание заказа
 CREATE_ORDER = "/api/v1/orders"
 # Получение заказа по его номеру
--- test_create_order_track.py
+++ test_create_order_track.py
@@ -2,5 +2,12 @@ import sender_stand_request
 import data
 
 def test_track_order():
-    response = sender_stand_request.get_track_order()
-    assert response.status_code == 200
+    create_order = sender_stand_request.post_new_order(data.order_body)
+    assert create_order.status_code in (200,201)
+
+    create_json = create_order.json()
+    assert "track" in create_json
+    track = create_json["track"]
+
+    track_order = sender_stand_request.get_track_order(track)
+    assert track_order.status_code == 200
